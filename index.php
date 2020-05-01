747a11b9d863a580904c52101f24f0077d2bcb1c
evgen pishev <evgewi4@gmail.com>
2020-05-01 13:04:35 +0300

    add files

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <header class="col-sm-12 col-md-12 col-lg-12">
+
+            </header>
+        </div>
+        <div class="row">
+            <nav class="col-sm-12 col-md-12 col-lg-12">
+                <?php include_once('pages/menu.php'); ?>
+            </nav>
+        </div>
+        <div class="row">
+            <section class="col-sm-12 col-md-12 col-lg-12">
+                <?php
+                if (isset($_GET['page'])) {
+                    $page = $_GET['page'];
+                    if ($page == 1) include_once('pages/home.php');
+                    if ($page == 2) include_once('pages/upload.php');
+                    if ($page == 3) include_once('pages/gallery.php');
+                    if ($page == 4) include_once('pages/registration.php');
+                }
+                ?>
+            </section>
+        </div>
+    </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ pages/menu.php
@@ -0,0 +1,10 @@
+<ul class="nav nav-pills">
+<li role="presentation" class="active"><a ref="index.php?page=1">Home</a></li>
+
+<li role="presentation"><a href="index.php?page=2">Upload</a></li>
+
+<li role="presentation"><a href="index.php?page=3">Gallery</a></li>
+
+<li role="presentation"><a href="index.php?page=4">Registration</a></li>
+
+<ul>
